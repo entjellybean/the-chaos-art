3de9b47f4c2ebdfe0c7184e62b7dd738630e4f9e
entjellybean <basakunal2003@gmail.com>
2025-02-10 14:33:05 +0100

    carousel kisaltma

--- app/gallery.js
+++ app/gallery.js
@@ -129,6 +129,34 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 
+document.addEventListener('DOMContentLoaded', function () {
+    const images = document.querySelectorAll('.lazy');
+
+    if ('loading' in HTMLImageElement.prototype) {
+        // Eğer tarayıcı native lazy loading destekliyorsa
+        images.forEach(img => {
+            img.src = img.dataset.src;
+        });
+    } else {
+        // Eğer desteklemiyorsa IntersectionObserver kullan
+        const observer = new IntersectionObserver(entries => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    const img = entry.target;
+                    img.src = img.dataset.src;
+                    img.classList.remove('lazy');
+                    observer.unobserve(img);
+                }
+            });
+        });
+
+        images.forEach(img => observer.observe(img));
+    }
+});
+
+
+
+
 
 
 
